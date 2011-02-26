4b0b94da7574013731a711af66bbb5056f8103fd
yaoxianfeng <yaoxianfeng@gmail.com>
2011-02-26 14:07:26 +0000

    PoEAA

---
+++ src/eaa/datasource/datamapper/Person.java
@@ -0,0 +1,26 @@
+package eaa.datasource.datamapper;
+
+import eaa.base.layersupertype.DomainObject;
+
+public class Person extends DomainObject {
+    private String lastName;
+    private String firstName;
+    private int    numberOfDependents;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public int getNumberOfDependents() {
+        return numberOfDependents;
+    }
+
+
+    public Person(Long id, String lastNameArg, String firstNameArg, int numDependentsArg) {
+        // TODO Auto-generated constructor stub
+    }
+}
